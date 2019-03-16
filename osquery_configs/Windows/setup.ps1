b6462d73748d9298ec0a5cedfe9f3c53b6e2f0cf
Ben Bornholm <cptofevilminions@Bens-MacBook-Pro.local>
2019-03-16 16:48:38 -0400

    Added Windows configs and seutp script

--- osquery_configs/Windows/setup.ps1
+++ osquery_configs/Windows/setup.ps1
@@ -1,15 +1,36 @@
-# Download OSquery agent
-Invoke-WebRequest https://pkg.osquery.io/windows/osquery-3.3.2.msi -OutFile %USERPROFILE%\AppData\Local\osquery-3.3.2.msi
+# Command line args
+param (
+  $osquery_version="3.3.2",
+  $osquery_flag_url="https://raw.githubusercontent.com/CptOfEvilMinions/QueryLab/master/osquery_configs/Windows/osquery.flags",
+  $osquery_conf_url="https://raw.githubusercontent.com/CptOfEvilMinions/QueryLab/master/osquery_configs/Windows/osquery-windows.conf"
+)
 
-# CD into user's TEMP folder
-cd %USERPROFILE%\AppData\Local
+Write-Output "Osquery version: $osquery_version"
+Write-Output "osquery.flags url: $osquery_flag_url"
+Write-Output "osquery.conf url: $osquery_conf_url"
+
+# Download OSquery agent
+Write-Output "Downloading OSquery $osquery_version"
+Invoke-WebRequest "https://pkg.osquery.io/windows/osquery-$osquery_version.msi" -OutFile $env:TEMP\osquery-$osquery_version.msi
+
 
 # Install OSquery
-.\osquery-3.3.2.msi /quiet
-
-# Download Windows osquery.flag
+Write-Output "Installing OSquery $osquery_version"
+Start-Process $env:TEMP\osquery-$osquery_version.msi /quiet -Wait
 
 # Download Windows osquery.conf
+Write-Output "Downloading osquery.conf"
+Invoke-WebRequest $osquery_conf_url -OutFile C:\ProgramData\osquery\osquery.conf
+
+# Download Windows osquery.flags
+Write-Output "Downloading osquery.flags"
+Invoke-WebRequest $osquery_flag_url -OutFile C:\ProgramData\osquery\osquery.flags
 
 # Restart service
+Write-Output "Restarting OSqueryD service"
 Restart-Service -Name osqueryd
+
+# Print service status
+Get-Service -Name osqueryd
+
+Write-Output "Done :) - QueryLab"
